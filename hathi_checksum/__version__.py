df2fbbe8a685af49569d0d7fb5f44f5362f5b85e
Borchers, Henry Samuel <hborcher@illinois.edu>
2019-03-21 13:50:16 -0500

    next iteration cycle

--- hathi_checksum/__version__.py
+++ hathi_checksum/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'HathiChecksumUpdater'
-__version__ = '0.1.1'
+__version__ = '0.1.2.dev0'
 __url__ = 'https://github.com/UIUCLibrary/HathiValidate'
 __author__ = 'University of Illinois at Urbana Champaign'
 __author_email__ = 'hborcher@illinois.edu'
